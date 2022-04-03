cacbb8b97d3fb132a51b540038b9cbd01f8ed564
Ramiro Balmaceda <ramirobj.25@gmail.com>
2022-04-03 14:17:06 -0500

    mods

--- pytradeautomate/feed.py
+++ pytradeautomate/feed.py
@@ -6,7 +6,11 @@ class Feed:
     def __init__(self, symbol, interval, limit):
         self.__client = Client(config.API_KEY, config.API_SECRET, tld='com')
         self.__klines = self.__client.get_klines(symbol=symbol, interval=interval, limit=limit)
-        
+        self.start_date = None
+        self.end_date = None
+        self.symbol = symbol
+        self.interval = interval
+
     def get_df_klines(self):
         return pd.DataFrame(self.__klines)
 
@@ -15,3 +19,6 @@ class Feed:
 
     def to_csv(self, filename):
         self.__df_klines.to_csv(filename)
+
+    def get_df_klines_interval(self):
+        return pd.DataFrame(self.__client.get_klines(symbol=self.symbol, interval=self.interval, startTime = self.start_date, endTime = self.end_date))
--- pytradeautomate/strategy.py
+++ pytradeautomate/strategy.py
@@ -23,43 +23,60 @@ class Strategy:
         cnt_operations = 0
         win_rate = 0
         loss_rate = 0
+        tendencia = None
         df_klines = pd.DataFrame(self.klines)
         df_klines['closed_price'] = df_klines.apply(lambda x: float(x[4]) ,axis=1)
         df_klines['lowest_price'] = df_klines.apply(lambda x: float(x[3]) ,axis=1)
+        df_klines['higest_price'] = df_klines.apply(lambda x: float(x[2]) ,axis=1)
         stdup, mean, stddown = ta.BBANDS(df_klines['closed_price'].values, timeperiod=timeperiod, nbdevup=nbdevup, nbdevdn=nbdevdn, matype=0)
         rsi = ta.RSI(df_klines['closed_price'].values, timeperiod=timeperiod)
-        
+        ma100 = ta.SMA(df_klines['closed_price'].values, timeperiod=100)
+        ma200 = ta.SMA(df_klines['closed_price'].values, timeperiod=200)
+        ma50 = ta.SMA(df_klines['closed_price'].values, timeperiod=50)
+        ema20 = ta.EMA(df_klines['closed_price'].values, timeperiod=20)
+        slowk, slowd = ta.STOCH(df_klines['higest_price'].values, df_klines['lowest_price'].values, df_klines['closed_price'].values, fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
+        macd, macdsignal, macdhist = ta.MACD(df_klines['closed_price'].values, fastperiod=12, slowperiod=26, signalperiod=9)
+
         operation = op(self.symbol)
         print(f'Balance inicial: {self.bac_account}')
-        feed_4h = fd(self.symbol, '4h', 500)
-        klines_4h = feed_4h.get_df_klines()
-        tendencia = self.get_tendencia(klines_4h)
+        
+        for i in df_klines.index:
+            date = datetime.datetime.fromtimestamp(df_klines[0][i]/1000)
+            '''feed_4h = fd(self.symbol, '30m', 500)
+            feed_4h.start_date = date-datetime.timedelta(days=3)
+            feed_4h.start_date = int(feed_4h.start_date.timestamp()*1000)            
+            feed_4h.end_date = df_klines[0][i]
+            feed_4h.end_date= df_klines[0][i]
+            klines_4h = feed_4h.get_df_klines_interval()
+            tendencia = self.get_tendencia(klines_4h)'''
+            #if tendencia == 'Ascendente':
 
-        if tendencia == 'Ascendente':
-            for i in df_klines.index:
-                date = datetime.datetime.fromtimestamp(df_klines[0][i]/1000).strftime("%Y-%m-%d %H:%M:%S")
+            if macd[i] < 0 and macdsignal[i] < 0 and macd[i] > macdsignal[i] and not operation.status:
+                self.bac_account = operation.buy(self.bac_account.account_balance, df_klines['closed_price'][i], self.bac_account)
+                print(f'{date} - {df_klines[4][i]} - {ma50[i]} - {ma200[i]}')
+                print(self.bac_account)
 
-                if df_klines['closed_price'][i] <= stddown[i] + (stddown[i] * 0.001) and not operation.status and self.bac_account.account_balance > 0:
-                    self.bac_account = operation.buy(self.bac_account.account_balance, df_klines['closed_price'][i], self.bac_account)
-                    print(f'{date} - {df_klines[4][i]} - {stdup[i]} - {mean[i]} - {stddown[i]}')
-                    print(self.bac_account)
+            elif df_klines['closed_price'][i] >= operation.buyPrice and df_klines['closed_price'][i] >= operation.buyPrice + (operation.buyPrice * 0.005) and operation.status:
+                self.bac_account = operation.sell(operation.get_operation_balance(), df_klines['closed_price'][i], self.bac_account)
+                self.current_balance = self.bac_account.account_balance
+                print(f'{date} - {df_klines[4][i]} - {ma50[i]} - {ma200[i]}')
+                cnt_operations += 1
+                win_rate += 1
+                print(f'Win operation')
 
-                elif df_klines['closed_price'][i] >= operation.buyPrice and df_klines['closed_price'][i] >= stdup[i] and operation.status:
-                    self.bac_account = operation.sell(operation.get_operation_balance(), df_klines['closed_price'][i], self.bac_account)
-                    self.current_balance = self.bac_account.account_balance
-                    print(f'{date} - {df_klines[4][i]} - {stdup[i]} - {mean[i]} - {stddown[i]}')
-                    cnt_operations += 1
-                    win_rate += 1
-                    print(f"Win Operation: {df_klines['closed_price'][i] }")
-
-                elif df_klines['closed_price'][i] <= operation.buyPrice and df_klines['lowest_price'][i] <= stddown[i] - (stddown[i]*0.001) and operation.status:
-                    self.bac_account = operation.sell(operation.get_operation_balance(), df_klines['closed_price'][i], self.bac_account)
-                    self.current_balance = self.bac_account.account_balance
-                    print(f'{date} - {df_klines[4][i]} - {stdup[i]} - {mean[i]} - {stddown[i]}')
-                    cnt_operations += 1
-                    loss_rate += 1
-                    print(f"Loss Operation: {df_klines['closed_price'][i] }")
+            elif df_klines['closed_price'][i] <= operation.buyPrice and df_klines['lowest_price'][i] <= operation.buyPrice - (operation.buyPrice*0.003) and operation.status:
+                self.bac_account = operation.sell(operation.get_operation_balance(), df_klines['closed_price'][i], self.bac_account)
+                self.current_balance = self.bac_account.account_balance
+                print(f'{date} - {df_klines[4][i]} - {stdup[i]} - {mean[i]} - {stddown[i]}')
+                cnt_operations += 1
+                loss_rate += 1
+                print(f"Loss Operation: {df_klines['closed_price'][i] }")
             
+            
+            self.last_price = df_klines['closed_price'][i]
+
+            #else:
+            #    pass
 
         print(f'Cantidad de operaciones: {cnt_operations}')
         print(f'Cantidad de operaciones ganadas: {win_rate}')
@@ -70,9 +87,8 @@ class Strategy:
             print(f'Balance actual de BUSD: {self.bac_account.account_balance}')
         else:
             print(f'Balance actual de BUSD: {operation.get_operation_balance()*self.last_price}')
-        print(f'{self.bac_account}')
 
-    def get_tendencia(df_klines):
+    def get_tendencia(self, df_klines):
         df_klines['closed_price'] = df_klines.apply(lambda x: float(x[4]) ,axis=1)
         sma = ta.SMA(df_klines['closed_price'].values, timeperiod=20)
         sma_actual = sma[len(sma)-1]
@@ -92,7 +108,6 @@ class Strategy:
                 stopPrice='{:.8f}'.format(round(stopPrice)),
                 timeInForce='GTC')
         return buyOrder
-
 
     def sellShort(self, sellQty, sellPrice, stopPrice):
         sellOrder = self.client.create_order(
@@ -114,7 +129,11 @@ if __name__ == "__main__":
     interval = '5m'
     limit = 1000
     feed = fd(symbol=symbol, interval=interval, limit=limit)
-    st = Strategy('BTCBUSD', feed.get_Client(), feed.get_df_klines())
+    feed.start_date = datetime.datetime.now() - datetime.timedelta(days=60)
+    feed.start_date = int(feed.start_date.timestamp()*1000)
+    feed.end_date = datetime.datetime.now() - datetime.timedelta(days=50)
+    feed.end_date = int(feed.end_date.timestamp()*1000)
+    st = Strategy('BTCBUSD', feed.get_Client(), feed.get_df_klines_interval())
     st.backtestBB(20, 2, 0)
 
 
